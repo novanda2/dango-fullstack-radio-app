0eb1863e96ac4903e8086a8ffdbc1786485fba1a
Novanda <nathoilla@indo-soft.com>
2021-03-18 08:06:17 +0700

    Adjust FrontendEditable

--- podcast/models.py
+++ podcast/models.py
@@ -11,23 +11,6 @@ class Host(models.Model):
         return self.first_name + ' ' + self.last_name
 
 
-# class Article(models.Model):
-#     title = models.CharField(max_length=200)
-#     content = RichTextField()
-
-#     def __str__(self):
-#         return self.title
-
-
-# class Audio(models.Model):
-#     title = models.CharField(max_length=200)
-#     link: models.CharField(
-#         max_length=200, default="https://share.transistor.fm/e/1493e91f/dark")
-
-#     def __str__(self):
-#         return self.title
-
-
 STATUS_CHOICES = (
     (0, 'draft'),
     (1, 'publish')
