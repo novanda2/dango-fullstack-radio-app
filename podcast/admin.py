faeab216d5d890790cb9a6e805e374fec519679a
Novanda Ahsan <novandaahsan1@gmail.com>
2021-03-18 07:12:46 +0700

    Add rendder_model in Template

--- podcast/admin.py
+++ podcast/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import Episode, Host
-from cms.admin.placeholderadmin import PlaceholderAdminMixin
+from cms.admin.placeholderadmin import PlaceholderAdminMixin, FrontendEditableAdminMixin
 
 
 @admin.register(Episode)
-class EpisodeAdmin(PlaceholderAdminMixin, admin.ModelAdmin):
+class EpisodeAdmin(FrontendEditableAdminMixin, PlaceholderAdminMixin, admin.ModelAdmin):
     list_display = ('title', 'host')
     list_filter = ('created',)
+    frontend_editable_fields = ('excerpt', 'podcast')
 
 
 @admin.register(Host)
