e46540c045fc1cdb838bb0dbc2eda5a54e116cc0
Novanda Ahsan <novandaahsan1@gmail.com>
2021-07-29 11:38:03 +0700

    add plugin: podcast list

--- dango/apps/podcast_cms_integration/models.py
+++ dango/apps/podcast_cms_integration/models.py
@@ -1,3 +1,4 @@
+from dango.apps.podcast.views import PodcastListView
 from django.db import models
 from cms.models import CMSPlugin
 from dango.apps.podcast.models import Episode, Host
@@ -5,7 +6,8 @@ from dango.apps.podcast.models import Episode, Host
 
 class PodcastPluginModel(CMSPlugin):
     episode = models.ForeignKey(Episode, on_delete=models.CASCADE)
-    host = models.ForeignKey(Host, on_delete=models.CASCADE, blank=True, null=True)
+    host = models.ForeignKey(
+        Host, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.episode.title
