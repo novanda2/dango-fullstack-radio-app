83dad48108ff408f5efd8f668d01511407cc28de
Novanda Ahsan <novandaahsan1@gmail.com>
2021-07-29 11:37:19 +0700

    cms worked together with multi language

--- dango/urls.py
+++ dango/urls.py
@@ -1,28 +1,39 @@
-from django.urls.conf import include, re_path
 from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls.static import static
+from django.urls import include, re_path
+
+from cms import views
+from cms.apphook_pool import apphook_pool
+from cms.appresolver import get_app_patterns
+from cms.constants import SLUG_REGEXP
+
 from django.contrib import admin
 from django.views.i18n import JavaScriptCatalog
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.i18n import i18n_patterns
 
-admin.autodiscover()
 
-urlpatterns = i18n_patterns(
-    re_path(r'^jsi18n/$', JavaScriptCatalog.as_view(), name='javascript-catalog'),
-)
+if settings.APPEND_SLASH:
+    regexp = r'^(?P<slug>%s)/$' % SLUG_REGEXP
+else:
+    regexp = r'^(?P<slug>%s)$' % SLUG_REGEXP
 
-urlpatterns += staticfiles_urlpatterns()
+if apphook_pool.get_apphooks():
+    # If there are some application urls, use special resolver,
+    # so we will have standard reverse support.
+    urlpatterns = get_app_patterns()
+else:
+    urlpatterns = []
 
-urlpatterns += i18n_patterns(
-    re_path(r'^', include(('dango.apps.podcast.urls', 'podcast'), namespace="podcast"), name="podcast"),
+
+urlpatterns.extend(i18n_patterns(
     re_path("admin/", admin.site.urls),
-    re_path(r'^i18n/', include('django.conf.urls.i18n')),
-)
-
-# This is only needed when using runserver.
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
+    re_path(r'^cms_login/$', views.login, name='cms_login'),
+    re_path(r'^cms_wizard/', include('cms.wizards.urls')),
+    re_path(r'^podcast/', include(
+        ('dango.apps.podcast.urls', 'podcast'),
+        namespace="podcast"), name="podcast"),
+    re_path(r'^jsi18n/$',
+            JavaScriptCatalog.as_view(),
+            name='javascript-catalog'),
+    re_path(regexp, views.details, name='pages-details-by-slug'),
+    re_path(r'^$', views.details, {'slug': ''}, name='pages-root'),
+))
